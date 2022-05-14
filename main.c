f96e9f6d0f4834d1f00ff28f019f7d1f2b85a39a
erickRochaIP <erickfr1011@gmail.com>
2022-05-14 17:20:48 -0300

    Implementar tiros

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 
 #include "utils.h"
 
+// Aqui declaramos os objetos e listas de objeto
 ObjetoJogo nave;
+ListaObjetos tiros;
+// TODO: Declarar lista de inimigos
+
 
 GLuint carregaTextura(char* arquivo) {
     GLuint idTextura = SOIL_load_OGL_texture(
@@ -19,27 +23,50 @@ GLuint carregaTextura(char* arquivo) {
     return idTextura;
 }
 
+void removeTirosFora(){
+    for (int i = 0; i < getSize(tiros); i++){
+        if (getObjetoJogo(tiros, i)->posicao.y > 100){
+            pop(&tiros, i);
+            return removeTirosFora();
+        }
+    }
+    return;
+}
+
+// Recebe um ObjetoJogo e printa ele na tela
+void desenhaObjeto(ObjetoJogo obj) {
+    glColor3f(0, 1, 0);
+    glEnable(GL_TEXTURE_2D);
+    // TODO: Fazer a textura funcionar
+    glBindTexture(GL_TEXTURE_2D, obj.idTextura);
+    glBegin(GL_TRIANGLE_FAN);
+        //glTexCoord2f(obj.posicao.x-5, obj.posicao.y-5);
+        glVertex3f(obj.posicao.x+obj.dimensoes.x, obj.posicao.y, 0); //baixo direito
+
+        //glTexCoord2f(obj.posicao.x-5, obj.posicao.y+5);
+        glVertex3f(obj.posicao.x+obj.dimensoes.x, obj.posicao.y+obj.dimensoes.y, 0); //cima direito
+
+        //glTexCoord2f(obj.posicao.x+5, obj.posicao.y+5);
+        glVertex3f(obj.posicao.x, obj.posicao.y+obj.dimensoes.y, 0); //cima esquerdo
+
+        //glTexCoord2f(obj.posicao.x+5, obj.posicao.y-5);
+        glVertex3f(obj.posicao.x, obj.posicao.y, 0); //baixo esquerdo
+    glEnd();
+    glDisable(GL_TEXTURE_2D);
+}
+
 // Callback: Cena deve ser desenhada
 void desenhaMinhaCena() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(0, 1, 0);
 
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, nave.idTextura);
-    glBegin(GL_TRIANGLE_FAN);
-        //glTexCoord2f(nave.posicao.x-5, nave.posicao.y-5);
-        glVertex3f(nave.posicao.x+nave.dimensoes.x, nave.posicao.y, 0); //baixo direito
+    // desenha nave
+    desenhaObjeto(nave);
 
-        //glTexCoord2f(nave.posicao.x-5, nave.posicao.y+5);
-        glVertex3f(nave.posicao.x+nave.dimensoes.x, nave.posicao.y+nave.dimensoes.y, 0); //cima direito
-
-        //glTexCoord2f(nave.posicao.x+5, nave.posicao.y+5);
-        glVertex3f(nave.posicao.x, nave.posicao.y+nave.dimensoes.y, 0); //cima esquerdo
-
-        //glTexCoord2f(nave.posicao.x+5, nave.posicao.y-5);
-        glVertex3f(nave.posicao.x, nave.posicao.y, 0); //baixo esquerdo
-    glEnd();
-    glDisable(GL_TEXTURE_2D);
+    // desenha tiros
+    for(int i = 0; i < getSize(tiros); i++)
+        desenhaObjeto(*getObjetoJogo(tiros, i));
+    
+    // TODO: desenhar inimigos
 
     glutSwapBuffers();
 }
@@ -60,7 +87,20 @@ void redimensionada(int width, int height) {
 void teclaPressionada(unsigned char key, int x, int y) {
     switch(key) {
         case 27:
+            freeLista(&tiros);
+            // TODO: liberar lista de inimigos
             exit(0);
+            break;
+        case 32:    // espaço
+            // cria um tiro e o adiciona na lista de tiros
+            ObjetoJogo novoTiro = new_ObjetoJogo(
+                new_Vetor(nave.posicao.x + nave.dimensoes.x/2, nave.posicao.y + nave.dimensoes.y),  // tiro sai do meio da nave
+                new_Vetor(0, 2),    // velocidade apenas na vertical
+                new_Vetor(1, 3),    // (largura, altura) do tiro
+                0   // TODO: Textura do tiro
+            );
+            append(&tiros, novoTiro);
+
             break;
 
         default:
@@ -72,10 +112,12 @@ void teclaPressionada(unsigned char key, int x, int y) {
 void setaPressionada(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_RIGHT:  //seta direita
+            // nave tem velocidade para direita ->
             nave.velocidade.x=0.5;
             nave.velocidade.y=0;
             break;
         case GLUT_KEY_LEFT: //seta esquerda
+            // nave tem velocidade para esquerda <-
             nave.velocidade.x=-0.5;
             nave.velocidade.y=0;
             break;
@@ -90,6 +132,7 @@ void setaNaoPressionada(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_RIGHT:  //seta direita ou esquerda
         case GLUT_KEY_LEFT:
+            // nave perde toda velocidade
             nave.velocidade.x=0;
             nave.velocidade.y=0;
             break;
@@ -101,51 +144,86 @@ void setaNaoPressionada(int key, int x, int y) {
 
 // Callback: atualiza valores dos objetos
 void atualizaCena(int valor){
-    //tratar para quando as coordenadas ultrapassarem o gortho
+
+    // atualiza posicao da nave
     nave.posicao.x += nave.velocidade.x;
     nave.posicao.y += nave.velocidade.y;
+    // TODO: Checar se posicao esta fora do gortho
 
+    // atualiza posicao dos tiros
+    for (int i = 0; i < getSize(tiros); i++){
+        ObjetoJogo * tiro = getObjetoJogo(tiros, i);
+
+        tiro->posicao.x += tiro->velocidade.x;
+        tiro->posicao.y += tiro->velocidade.y;
+    }
+
+    // remove todos os tiros que sairam da tela
+    removeTirosFora();
+
+    // TODO: atualizar posicao e velocidade dos inimigos
+
+    // TODO: remover tiros e inimigos que colidiram
+
+    // TODO: checar se inimigo alcancou a nave e terminar o jogo
+
+    // manda redesenhar a cena
     glutPostRedisplay();
 
+    // manda chamar esta função novamente daqui 0,33s
     glutTimerFunc(33, atualizaCena, 0);
 }
 
-
+// atribui valores iniciais dos ObjetoJogo
 void inicializa() {
     glClearColor(1, 1, 1, 1); // branco
 
+    // cria nave
     nave = new_ObjetoJogo(
         new_Vetor(50, 5),   // posicao
         new_Vetor(0, 0),    // velocidade inicial
         new_Vetor(6, 6),    // tamanho
         carregaTextura("pixel-art-spacecraft.png")  // textura
     );
+
+    // cria lista vazia de tiros
+    tiros = new_ListaObjetos();
+
+    // TODO: criar lista cheia de inimigos
 }
 
 int main(int argc, char** argv) {
-   glutInit(&argc, argv);
+    glutInit(&argc, argv);
 
-   glutInitContextVersion(1, 1);
-   glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
+    glutInitContextVersion(1, 1);
+    glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
 
-   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-   glutInitWindowSize(500, 500);
-   glutInitWindowPosition(100, 100);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(100, 100);
 
-   glutCreateWindow("Movimentacao Horizontal");
-
-   glutDisplayFunc(desenhaMinhaCena);
-   glutReshapeFunc(redimensionada);
-   
-   glutKeyboardFunc(teclaPressionada);
-   glutSpecialFunc(setaPressionada);
-   glutSpecialUpFunc(setaNaoPressionada);
+    glutCreateWindow("Movimentacao Horizontal");
 
 
-   glutTimerFunc(33, atualizaCena, 0);
+    // Callback de desenhar cena
+    glutDisplayFunc(desenhaMinhaCena);
+    glutReshapeFunc(redimensionada);
 
-   inicializa();
+    // Callback de input do usuário
+    glutKeyboardFunc(teclaPressionada);
+    glutSpecialFunc(setaPressionada);
+    glutSpecialUpFunc(setaNaoPressionada);
 
-   glutMainLoop();
-   return 0;
+    // Callback para atualizacao de valores
+    glutTimerFunc(33, atualizaCena, 0);
+
+    // atribui valores iniciais dos ObjetoJogo
+    inicializa();
+
+    glutMainLoop();
+
+    freeLista(&tiros);
+    // TODO: liberar lista de inimigos
+
+    return 0;
 }
